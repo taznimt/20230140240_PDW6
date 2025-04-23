13aa84fea45a5a6eaada1d20d68357087fc902cf
taznimt <taznimanwar06@gmail.com>
2025-04-23 14:42:13 +0700

    first commit

---
+++ jsprak.js
@@ -0,0 +1,15 @@
+const video = document.getElementById("video");
+const audio = document.getElementById("audio");
+const gambar = document.getElementById("gambar");
+
+video.addEventListener("click", function () {
+  alert("ini video ceramah");
+});
+
+audio.addEventListener("click", function () {
+  alert("ini lagu bts");
+});
+
+gambar.addEventListener("click", function () {
+  alert("ini gambar ustad");
+});
